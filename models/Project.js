d5fd09e0dcab58c51f2f88faf9c7f0976a45863e
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-06 11:30:21 -0800

    Touches up Project model and Renames 'users' to 'User' in User model

--- models/Project.js
+++ models/Project.js
@@ -1,25 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProjectSchema = new Schema({
-  title: {
+const PositionSchema = new Schema({
+  role: {
     type: String,
     required: true
+  },
+  filled: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+const ProjectSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  title: {
+    type: String,
+    required: [true, "Please enter a project title"]
   },
   description: {
     type: String,
-    required: true
+    required: [true, "Please add a brief description"]
   },
   github: {
-    type: String,
-    required: true
+    type: String
   },
-  emptyPositions: [{ type: String }],
+  positions: [PositionSchema],
   collaborators: [{ type: Schema.Types.ObjectId, ref: 'users' }],
-  goals: [{ type: Schema.Types.ObjectId, ref: 'goals' }],
-
+  goals: [{ type: Schema.Types.ObjectId, ref: 'Goal' }]
 }, {
   timestamps: true
 });
 
-module.exports = Project = mongoose.model("projects", ProjectSchema);
+module.exports = Project = mongoose.model("Project", ProjectSchema);
