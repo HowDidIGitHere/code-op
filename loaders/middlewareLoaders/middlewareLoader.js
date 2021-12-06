0d19978c9ecac6ed35c4799482af57f62df0652b
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-06 02:47:13 -0800

    Finishes setting up middleware loaders excluding routers and errors

--- loaders/middlewareLoaders/middlewareLoader.js
+++ loaders/middlewareLoaders/middlewareLoader.js
@@ -2,11 +2,11 @@ module.exports = {
   customMiddlewareLoader: require("./customMiddlewareLoader"),
   errorMiddlewareLoader: require("./errorMiddlewareLoader"),
   routersLoader: require("./routersLoader"),
-  // thirdPartyMiddlewareLoader: require("./thirdPartyMiddlewareLoader"),
+  thirdPartyMiddlewareLoader: require("./thirdPartyMiddlewareLoader"),
   init: async (app) => {
-    // await this.thirdPartyMiddlewareLoader(app);
+    await this.thirdPartyMiddlewareLoader(app);
     await this.customMiddlewareLoader(app);
-    await this.customMiddlewareLoader(app);
+    await this.routersLoader(app);
     await this.errorMiddlewareLoader(app);
     return app;
   }
--- loaders/middlewareLoaders/thirdPartyMiddlewareLoader.js
+++ loaders/middlewareLoaders/thirdPartyMiddlewareLoader.js
@@ -1,1 +1,52 @@
 // TO ADD ADDITIONAL MIDDLEWARE LATER
+
+const device = require("express-device");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const mongoSanitize = require("express-mongo-sanitize");
+const morgan = require("morgan");
+const path = require("path");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const { catchAsync } = require("../../utils");
+
+module.exports = catchAsync( async app => {
+  // Sets security http headers
+  app.use(helmet());
+
+  // Logs requests
+  app.use(morgan("dev"));
+
+  // Limits user requests so we don't get swamped
+  app.use("/", rateLimit({
+    max: 150,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests. Please try again in an hour!"
+  }));
+
+  // Passport for session tokens
+  app.use(passport.initialize());
+  require("./config/passport")(passport);
+
+  // Body parser: reading data from body into req.body
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  // Sanitize data against NoSQL query injections
+  app.use(mongoSanitize());
+
+  // Sanitize data against XSS
+  app.use(xss());
+
+  // Prevent parameter pollution
+  app.use(hpp({
+    whitelist: [
+      "substring"
+    ]
+  }));
+
+  return app;
+});
