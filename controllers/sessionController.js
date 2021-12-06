2478fa7eb987701f2a62efb14750611381560334
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 03:02:20 -0800

    Refactor login functionality into session controller

---
+++ controllers/sessionController.js
@@ -0,0 +1,43 @@
+const validateLoginInput = require("../../validation/login");
+
+export const login = (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        // no user with this email found
+        errors.email = "User not found";
+        return res.status(404).json(errors);
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = { id: user.id, username: user.username };
+            
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Key expires in 1 hr
+              { expiresIn: 3600 },
+              (error, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            errors.password = "Incorrect password";
+            return res.status(400).json(errors);
+          }
+        });
+    });
+}
