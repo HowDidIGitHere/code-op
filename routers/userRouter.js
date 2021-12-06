f6de2cadff59d189533b1d492db6bde81e5f5890
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 15:05:37 -0800

    Revert userRouter to pre testing state

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
-router.post("/signup", authController.protect, authController.signup);
+router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 module.exports = router;
