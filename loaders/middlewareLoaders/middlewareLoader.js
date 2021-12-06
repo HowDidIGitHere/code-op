613861ad9a863d685c5e798a4475da445e9080dc
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-05 23:17:35 -0800

    Generates skeleton loader files for middleware

---
+++ loaders/middlewareLoaders/middlewareLoader.js
@@ -0,0 +1,13 @@
+module.exports = {
+  customMiddlewareLoader: require("./customMiddlewareLoader"),
+  errorMiddlewareLoader: require("./errorMiddlewareLoader"),
+  routersLoader: require("./routersLoader"),
+  thirdPartyMiddlewareLoader: require("./thirdPartyMiddlewareLoader"),
+  init: async (app) => {
+    await this.thirdPartyMiddlewareLoader(app);
+    await this.customMiddlewareLoader(app);
+    await this.customMiddlewareLoader(app);
+    await this.errorMiddlewareLoader(app);
+    return app;
+  }
+};
