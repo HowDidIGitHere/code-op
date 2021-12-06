abe44677169fda672adf50f43bdf4fb4ddaba3ad
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-06 09:52:57 -0800

    Moves login route to userRouter

--- routers/sessionRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authController = require("../controllers/authController");
-
-router.post("/login", authController.login);
-// router.post("/logout", authController.protect, authController.logout);
-
-module.exports = router;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 
 module.exports = router;
