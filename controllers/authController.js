6d22638b404444e49d64f595d981eae6ed3400c8
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 04:29:54 -0800

    Finalize transfer of signup functionality into AuthController

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,59 +2,38 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
 const catchAsync = require('../utils/catchAsync');
-const validateLoginInput = require("../validation/login");
 
 const User = require("../models/User")
+
+const validateSignupInput = require("../validation/signup");
+const validateLoginInput = require("../validation/login");
+
 
 exports.signup = catchAsync( async (req, res) => {
   const { errors, isValid } = validateSignupInput(req.body);
 
-  if (!isValid) {
+  if (!isValid)
     return res.status(400).json(errors);
-  }
+  
+  const user = await User.findOne({ email: req.body.email });
 
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        // user already exists
-        errors.email = "Email already exists";
-        return res.status(400).json(errors);
-      } else {
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password
-        });
+  if (user)
+    return res.status(400).json({ message: 'An error occurred' });
+  
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
 
-        bcrypt.genSalt(10, (error, salt) => {
-          bcrypt.hash(newUser.password, salt, (error, hash) => {
-            if (error) { throw error }
-            newUser.password = hash;
-            newUser.save()
-              .then(user => {
-                const payload = {
-                  username: user.username,
-                  email: user.email,
-                  id: user.id
-                }
+  await newUser.save();
 
-                jwt.sign(
-                  payload,
-                  keys.secretOrKey,
-                  // Key expires in 1 hr
-                  { expiresIn: 3600 },
-                  (error, token) => {
-                    res.json({
-                      success: true,
-                      token: "Bearer " + token
-                    });
-                  });
-              })
-              .catch(error => console.log(error))
-          });
-        });
-      }
-    });
+  const payload = { id: user.id, username: user.username };
+  const token = jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 });
+  return res.json({
+    success: true,
+    token: 'Bearer ' + token
+  });
 });
 
 exports.login = catchAsync( async (req, res) => {
