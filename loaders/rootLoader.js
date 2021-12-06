cca08df2e315ca4914396bffba41a401a6c8246d
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-05 21:41:30 -0800

    Creates root loader for easy access to all loaders

---
+++ loaders/rootLoader.js
@@ -0,0 +1,18 @@
+const databaseLoader = require("./databaseLoader");
+const { catchAsync } = require("../utils");
+const middlewareLoaders = require("./middlewareLoaders");
+const processErrorHandlingLoader = require("./processErrorHandlingLoaders");
+
+const init = catchAsync( async data => {
+  const databaseConnection = await databaseLoader();
+  await processErrorHandlingLoader();
+  await thirdpartyMiddlewareLoader(data.app);
+  await middlewareLoaders.init(data.app);
+});
+
+module.exports = {
+  databaseLoader,
+  init,
+  middlewareLoaders,
+  processErrorHandlingLoader
+};
