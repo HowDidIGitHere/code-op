050618db12997b27fda78f0bbe890bb307cd0450
kenny <kcheng16@gmail.com>
2021-12-06 11:32:29 -0800

    frontend: projects component: add update component; update index.js

---
+++ frontend/src/components/projects/projects_update_component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ProjectsUpdate extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  componentWillMount() {
+    // this.props.fetchTweets();
+  }
+
+  render() {
+    return(
+      <div className="projects-update">
+
+      </div>
+    )
+  }
+}
+
+export default ProjectsUpdate
---
+++ frontend/src/components/projects/projects_update_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import ProjectsUpdate from './projects_update_component';
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsUpdate);
