761b087fa05a07f89193326be7c0803475b3aeca
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 04:18:43 -0800

    Begin transferring signup logic into authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,57 @@ const validateLoginInput = require("../validation/login");
 
 const User = require("../models/User")
 
+exports.signup = catchAsync( async (req, res) => {
+  const { errors, isValid } = validateSignupInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // user already exists
+        errors.email = "Email already exists";
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (error, salt) => {
+          bcrypt.hash(newUser.password, salt, (error, hash) => {
+            if (error) { throw error }
+            newUser.password = hash;
+            newUser.save()
+              .then(user => {
+                const payload = {
+                  username: user.username,
+                  email: user.email,
+                  id: user.id
+                }
+
+                jwt.sign(
+                  payload,
+                  keys.secretOrKey,
+                  // Key expires in 1 hr
+                  { expiresIn: 3600 },
+                  (error, token) => {
+                    res.json({
+                      success: true,
+                      token: "Bearer " + token
+                    });
+                  });
+              })
+              .catch(error => console.log(error))
+          });
+        });
+      }
+    });
+});
+
 exports.login = catchAsync( async (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
 
