7564b5d5e7bbafa18024d21d757de3674cc4a534
ntademaru <ntademaru@gmail.com>
2021-12-06 14:23:40 -0800

    Main page layout with placeholders

---
+++ frontend/src/components/main/project_listing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectListing = () => {
+  return(
+    <div className='listing-component'>
+      <h1>Project Name</h1>
+      <p>Please join this super cool project. We want to make something awesome and need awesome people to help us</p>
+    </div>
+  )
+}
+
+export default ProjectListing
