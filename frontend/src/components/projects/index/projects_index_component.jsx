c095c0fb170dc3a907a65c91ed831a66586e3b44
ntademaru <ntademaru@gmail.com>
2021-12-06 17:05:49 -0800

    Index project page columns

--- frontend/src/components/projects/index/projects_index_component.jsx
+++ frontend/src/components/projects/index/projects_index_component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './project_index.css'
 import CategoryChecklist from './category_checklist';
+import IndexProjectListing from './index_project_listing';
 
 class ProjectsIndex extends React.Component {
   constructor(props) {
@@ -25,6 +26,18 @@ class ProjectsIndex extends React.Component {
             <CategoryChecklist />
             <CategoryChecklist />
           </div>
+        </div>
+        <div className='right-index-listings'>
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
+          <div className='index-item'><IndexProjectListing/></div>          
         </div>
       </div>
     )
