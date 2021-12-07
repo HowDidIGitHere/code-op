d897ab64123e45df68c8f2d9afee494ff1f1ae65
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 19:47:11 -0800

    Begin creating signup service

--- services/UserService.js
+++ services/UserService.js
@@ -5,10 +5,38 @@ const catchAsync = require("../utils/catchAsync");
 const Service = require('../services/Service');
 
 const validateLoginInput = require('../validation/login');
+const validateSignupInput = require('../validation/signup');
 
 class UserService extends Service {
   constructor(model) {
     super(model);
+  }
+
+  async signup(userInfo) {
+    const { errors, isValid } = validateSignupInput(req.body);
+    const error = {
+      error: true, 
+      messgae: 'Invalid email or password',
+      statusCode: 400
+    }
+
+    if (!isValid)
+      return  error;
+    
+    const { docs } = await this.model.findOne({ fields: { email: req.body.email } });
+  
+    if (user)
+      return res.status(400).json({ message: 'An error occurred' });
+    
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+    });
+  
+    const payload = { id: newUser.id, username: newUser.username };
+    const token = jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 });
   }
 
   async login(userInfo) {
