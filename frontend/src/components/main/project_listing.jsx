86527121c2c42e947e863cfb5a649b0a2f97fd56
ntademaru <ntademaru@gmail.com>
2021-12-07 09:37:08 -0800

    add ... feature to listings for description

--- frontend/src/components/main/project_listing.jsx
+++ frontend/src/components/main/project_listing.jsx
@@ -4,7 +4,7 @@ const ProjectListing = () => {
   return(
     <div className='listing-component'>
       <h1>Project Name</h1>
-      <p>Please join this super cool project. We want to make  something super cool and need cool people help us. Please apply and we will decide if youre worthy</p>
+      <p>Please join this super cool project. There are not enough character or words in this yet. We want to make  something super cool and need cool people help us. Please apply and we will decide if youre worthy</p>
     </div>
   )
 }
