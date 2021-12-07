3e1da89bf07066113fe449fd515c224a42e39df6
Abraham Fong <abisfong@ucsc.edu>
2021-12-06 23:57:08 -0800

    Connect goalRouter to app

--- loaders/middlewareLoaders/routersLoader.js
+++ loaders/middlewareLoaders/routersLoader.js
@@ -1,8 +1,10 @@
 const userRouter = require("../../routers/userRouter");
 const projectRouter = require("../../routers/projectRouter");
+const goalRouter = require("../../routers/goalRouter");
 
 module.exports = app => {
   app.use("/api/users", userRouter);
   app.use("/api/projects", projectRouter);
+  app.use("/api/goals", goalRouter);
   return app;
 };
