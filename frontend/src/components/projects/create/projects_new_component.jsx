2882f5f7b881042ca9fad3cc0f74758057f09519
ntademaru <ntademaru@gmail.com>
2021-12-07 18:10:17 -0800

    updated create page

--- frontend/src/components/projects/create/projects_new_component.jsx
+++ frontend/src/components/projects/create/projects_new_component.jsx
@@ -9,6 +9,7 @@ class ProjectsNew extends React.Component {
     this.state = {
       title: "",
       desctiption: "",
+      github: "",
     }
     this.handleSubmit=this.handleSubmit.bind(this)
   }
@@ -34,38 +35,39 @@ class ProjectsNew extends React.Component {
     return(
       <div className='create-project'>
         <form className='create-project-form' onSubmit={this.handleSubmit}>
+          <div className='main-form'>
           <div className='form-header'>
             <p>Create a New Project</p>
           </div>
           <div className='form-body'>
-            <div className='left-form'>
+            <div className='user-input'>
+            <label className='input-field'>Project Title</label>
             <input className='project-title'
               type="text"
-              placeholder="Add your title"
               value={this.state.title}
               onChange={this.update('title')}
             />
             <br />
+            <label className='input-field'>Github Link</label>
             <input className='project-git'
               type="text"
-              placeholder="Add your Github repository"
-              // value={this.state.title}
-              // onChange={this.update('title')}
+              value={this.state.github}
+              onChange={this.update('github')}
             />
             <br />
+            <div className='user-checks'>
+              <CategoryChecklist />
+              <CategoryChecklist />
+              <CategoryChecklist />
+            </div>
+            <label className='input-field'>Add a description of your project</label>
             <textarea 
               className='project-description'
-              placeholder="Tell everyone what your project is about"
               value={this.state.description}
               onChange={this.update('description')}
             />
             </div>
-            <div className='right-form'>
-              <CategoryChecklist />
-              <CategoryChecklist />
-              <CategoryChecklist />
-              <CategoryChecklist />
-            </div>
+          </div>
           </div>
           <div className='form-footer'>
             <button className='save-button' type='submit'>Save</button>
