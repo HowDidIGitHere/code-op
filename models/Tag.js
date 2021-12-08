45fa652ea2d611ac1dee93d705fd49321c7a656b
Abraham Fong <abisfong@ucsc.edu>
2021-12-07 17:42:38 -0800

    Begin functionality for multiple insertions of Tags for same Project

--- models/Tag.js
+++ models/Tag.js
@@ -23,6 +23,22 @@ class Tag {
       timestamps: true
     });
     
+    TagSchema.pre('save', function(next) {
+      console.log(this);
+      const { name } = this._conditions;
+      const names = name.split(' ').splice(1);
+      if (names.length > 1) {
+        // const namesList = tags.split(',');
+        // const insertions = tagsList.map( tag => {
+        //   return { name: tag,  }
+        // });
+        // this._conditions._id = tagDocs.map( tagDoc => tagDoc.it)
+        // delete this._conditions.tags; 
+      }
+      return;
+      // next();
+    });
+    
     return mongoose.models.Tag || mongoose.model("Tag", TagSchema);
   }
 
