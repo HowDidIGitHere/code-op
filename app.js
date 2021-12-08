a7ea0ed13626040a30aaad005cddbe4b556a924f
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-07 20:11:49 -0800

    Tracks keys

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 const loaders = require("./loaders/rootLoader");
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const startServer = () => {
   loaders.init({ app });
