a7654a61dbbe01930d13a48d2f215998f2f68949
nicoletademaru <89610817+nicoletademaru@users.noreply.github.com>
2021-12-07 18:31:44 -0800

    Revert "Style create form"

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -26,7 +26,7 @@ class NavBar extends React.Component {
         return (
             <div>
                 <Link to={'/projects'}>All Projects</Link>
-                <Link to={'/projects/new'}>Create a Project</Link>
+                <Link to={'/new_project'}>Create a Tweet</Link>
                 <Link to={'/profile'}>Profile</Link>
                 <button onClick={this.logoutUser}>Logout</button>
             </div>
