e5e7841fd8f8f752f33bcd4ef7d387d73a5e255c
ntademaru <ntademaru@gmail.com>
2021-12-08 14:53:49 -0800

    Finished main page for real

--- frontend/src/components/main/project_listing_row.jsx
+++ frontend/src/components/main/project_listing_row.jsx
@@ -16,19 +16,19 @@ class ProjectListingRow extends React.Component {
           <img src="https://res.cloudinary.com/dzixdb9eu/image/upload/v1639001054/unsplash_EzYq1HOl5_8_y8slio.png" alt="pic" />
           <div className='first-project-summary'>
             <div className='testing'>
-              <p className='para'>Delectus pericula id sed. Eu mei affert tritani tacimates, probatus atomorum.</p>
-              <p className='quote'>“Qui alii definiebas ullamtorquatos voluptaria, te clita mollis salutatus pri.”
+              <p className='para'>Delectus pericula id sed. Eu mei affert tritani tacimates, probatus atomorum.Veniam percipit patrioque est an, usu in denique nominavi intellegam, quaestio vituperata at pro.</p>
+              <p className='quote'>“Qui alii definiebas ullamtorquatos voluptaria, te clita mollis salutatus pri. In solet homero dissentiet duo.”
               </p>
               <p className='quoter'>-Karina, Software Developer</p>
-              <p className='para'>Vel regione persequeris no. Ne cibo autem modus usu. Qui te libris antiopam patrioque. Pro paulo tim. </p>
+              <p className='para'>Vel regione persequeris no. Ne cibo autem modus usu. Qui te libris antiopam patrioque. Pro paulo tim.</p>
             </div>
           </div>
         </div>
         <div className='second-project'>
           <div className='second-project-summary'>
             <div className='testing'>
-              <p className='para'>Lorem ipsum dolor sit amet, sit an blandit omnesque, porro petentium.</p>
-              <p className='quote'>Ea per esse dicam, duo eu feugiat torquatos voluptaria, te clita mollis salutatus pri.”
+              <p className='para'>Lorem ipsum dolor sit amet, sit an blandit omnesque, porro petentium. Cu ius saepe laudem quidam. Eu falli congue repudiare pro. </p>
+              <p className='quote'>Ea per esse dicam, duo eu feugiat torquatos voluptaria, te clita mollis salutatus pri. Ad nemore electram persecuti per, has te verear diceret accusata.”
               </p>
               <p className='quoter'>-Peter, UX Designer</p>
             </div>
