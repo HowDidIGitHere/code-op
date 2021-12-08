035231d90889dbccec2d5d4bb56099e26057c58e
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-08 15:23:44 -0800

    Adds goals and diagrams to entities reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
+import GoalsReducer from "./goals_reducer";
 import ProjectsReducer from "./projects_reducer";
 import UsersReducer from "./users_reducer"
+import DiagramsReducer from "./diagrams_reducer";
 
 export default combineReducers({
   projects: ProjectsReducer,
   users: UsersReducer,
+  goals: GoalsReducer,
+  diagrams: DiagramsReducer
 })
