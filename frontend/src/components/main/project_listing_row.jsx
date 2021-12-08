60cf4b739f2f8bd8000144010e62205a8467cca5
ntademaru <ntademaru@gmail.com>
2021-12-08 14:44:15 -0800

    Finished main page

--- frontend/src/components/main/project_listing_row.jsx
+++ frontend/src/components/main/project_listing_row.jsx
@@ -16,12 +16,24 @@ class ProjectListingRow extends React.Component {
           <img src="https://res.cloudinary.com/dzixdb9eu/image/upload/v1639001054/unsplash_EzYq1HOl5_8_y8slio.png" alt="pic" />
           <div className='first-project-summary'>
             <div className='testing'>
-              <p>Delectus pericula id sed. Eu mei affert tritani tacimates, probatus atomorum.</p>
-              <p>“Qui alii definiebas ullamtorquatos voluptaria, te clita mollis salutatus pri.” -Karina, Software Developer
+              <p className='para'>Delectus pericula id sed. Eu mei affert tritani tacimates, probatus atomorum.</p>
+              <p className='quote'>“Qui alii definiebas ullamtorquatos voluptaria, te clita mollis salutatus pri.”
               </p>
-              <p>Vel regione persequeris no. Ne cibo autem modus usu. Qui te libris antiopam patrioque. Pro paulo tim. </p>
+              <p className='quoter'>-Karina, Software Developer</p>
+              <p className='para'>Vel regione persequeris no. Ne cibo autem modus usu. Qui te libris antiopam patrioque. Pro paulo tim. </p>
             </div>
           </div>
+        </div>
+        <div className='second-project'>
+          <div className='second-project-summary'>
+            <div className='testing'>
+              <p className='para'>Lorem ipsum dolor sit amet, sit an blandit omnesque, porro petentium.</p>
+              <p className='quote'>Ea per esse dicam, duo eu feugiat torquatos voluptaria, te clita mollis salutatus pri.”
+              </p>
+              <p className='quoter'>-Peter, UX Designer</p>
+            </div>
+          </div>
+          <img src="https://res.cloudinary.com/dzixdb9eu/image/upload/v1639001054/unsplash_EzYq1HOl5_8_y8slio.png" alt="pic" />
         </div>
       </div>
     )
