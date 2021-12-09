1222f2137e612650a1934ce8205addc9f1263ab7
ntademaru <ntademaru@gmail.com>
2021-12-08 17:29:33 -0800

    finish styling dropdown buttons

--- frontend/src/components/projects/index/category_dropdown.jsx
+++ frontend/src/components/projects/index/category_dropdown.jsx
@@ -4,9 +4,8 @@ const CategoryDropdown = ({category}) => {
     let testArr = ['test', 'testing', 'tester', 'test1', 'testfile', 'testie'];
   return (
     <div className='category-filter-containers'>
-      <i class="fas fa-chevron-down"></i>
       <button className='dropdownbtn'>
-        <p>{category}</p><i className="fas fa-chevron-down"></i></button>
+        <p>{category}</p><i class="fas fa-chevron-down fa-xs"></i></button>
       {/* <div className='checklist'>
         { testArr.map((item) => { 
           return (
