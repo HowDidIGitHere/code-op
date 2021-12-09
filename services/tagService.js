2b634d59a4b513213bf193736da5aa72bf69319e
Abraham Fong <abisfong@ucsc.edu>
2021-12-09 03:43:31 -0800

    Create special query to get all tag names/categories

--- services/tagService.js
+++ services/tagService.js
@@ -1,27 +1,44 @@
 const Service = require('./Service');
 const Tag = require('../models/Tag');
+const tagNames = require('../utils/tags');
 
 class TagService extends Service {
   constructor(model) {
     super(model);
+    this.get = this.getWrapper(this.get);
   }
 
-  async create(data) {
-    console.log(data);
+  getWrapper = get => async (query) => {
+    const { categories } = query;
+    const selection = { _id: 'tags' };
+
+    if (!categories)
+      return await get(query);
+
+    if (Array.isArray(categories))
+      categories.forEach(category => {
+        if (tagNames[category])
+          selection[category] = tagNames[category];
+      });
+    else if (categories === 'all')
+      return tagNames;
+    return selection;
+  }
+
+  create = async (data) => {
     let doc;
     if (data.names) {
-      doc = {}
-      // const names = data.names.split(',');
-      // doc = await this.model.insertMany(names.map(name => {
-      //   return { name, it: data.it, modelType: data.modelType }
-      // }));
+      const names = data.names.split(',');
+      doc = await this.model.insertMany(names.map(name => {
+        return { name, it: data.it, modelType: data.modelType }
+      }));
     } else {
       doc = await this.model.create(data);
     }
 
     return {
       status: 201,
-      doc,
+      doc
     };
   }
 }
