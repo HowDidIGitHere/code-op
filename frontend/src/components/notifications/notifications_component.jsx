09aab237768b47d4b3d8194aa50f7b210212f7cb
kenny <kcheng16@gmail.com>
2021-12-09 12:06:56 -0800

    notifications: add accept request

--- frontend/src/components/notifications/notifications_component.jsx
+++ frontend/src/components/notifications/notifications_component.jsx
@@ -9,16 +9,22 @@ class Notifications extends React.Component{
     }
   }
 
-componentDidMount(){
-  this.props.fetchRequests(this.props.currentUser)
-    .then(res => {
-      this.setState({requests: Object.values(res.requests)});
-    }
-    )
-}
+  componentDidMount(){
+    this.props.fetchRequests(this.props.currentUser)
+      .then(res => {
+        this.setState({requests: Object.values(res.requests)});
+      }
+      )
+  }
+
+  handleAccept(projectId, senderId, requestId){
+    this.props.fetchProject(projectId)
+      .then(result => this.props.updateProject({collaborators: result.collaborators.push(senderId)}))
+      .then(() => this.props.deleteRequest(requestId))
+  }
 
   render(){
-    console.log("render state", this.state)
+    // console.log("render state", this.state)
     return(
       <div className="notifications">
         {this.state.requests.map((request, idx) =>
@@ -37,8 +43,11 @@ componentDidMount(){
                 <div>{request.message}</div>
               </div>
               <div className="note-buttons">
-                <button className="accept" onClick={}>Accept</button>
-                <button className="decline">Decline</button>
+                <button className="accept" 
+                  onClick={() => handleAccept(request.projectId, request.senderId, request._id)}
+                >Accept
+                </button>
+                <button className="decline" onClick={() => this.props.deleteRequest(request._id)}>Decline</button>
               </div>
             </div>
 
--- frontend/src/components/notifications/notifications_container.js
+++ frontend/src/components/notifications/notifications_container.js
@@ -1,7 +1,8 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { createGoal } from "../../actions/goal_actions";
-import { fetchRequests } from "../../actions/request_actions";
+import { fetchRequests, deleteRequest} from "../../actions/request_actions";
+import { fetchProject, updateProject } from "../../actions/project_actions";
 import Notifications from "./notifications_component";
 
 const mSTP = (state, ownProps) => {
@@ -12,7 +13,10 @@ const mSTP = (state, ownProps) => {
 
 const mDTP = (dispatch, ownProps) => {
   return {
-    fetchRequests: (recipientId) => dispatch(fetchRequests(recipientId))
+    fetchRequests: (recipientId) => dispatch(fetchRequests(recipientId)),
+    fetchProject: (id) => dispatch(fetchProject(id)),
+    updateProject: (data) => dispatch(updateProject(data)),
+    deleteRequest: (requestId) => dispatch(deleteRequest(requestId))
   }
 }
 
