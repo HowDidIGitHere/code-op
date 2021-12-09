783c70d5e0f010f551e934155e52c79597f2e6dd
ntademaru <ntademaru@gmail.com>
2021-12-09 03:10:14 -0800

    added more to the request form

--- frontend/src/components/requests/project_request.jsx
+++ frontend/src/components/requests/project_request.jsx
@@ -17,12 +17,13 @@ class ProjectRequest extends React.Component {
     return (
       <form className='project-request' >
         <div className='form-greeting'>
-          <p>Hi {user.username},</p>
-          <p>Would you like to join {project.title}?</p>
+          <p>Hi {user.username}!</p>
+          <p>Would you like to send a request to join {project.title}?</p>
         </div>
-        <div>
-          
+        <div className='roles'>
+          <p>Please choose which role you would like to fill: </p>
         </div>
+        <button type='submit'>Send Request</button>
       </form>
     )
   }
