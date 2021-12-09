12e79f573bdbb004b6008887c8aadcbcb82a6529
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-09 02:31:15 -0800

    Craetes api calls for requests and remvoes single get backend route

--- routers/requestRouter.js
+++ routers/requestRouter.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 // Restrict only to recipient
 router.get("/", protect, restrict, RequestController.get);
-router.get("/:id", protect, restrict, RequestController.getSingle);
 router.delete("/:id", protect, restrict, RequestController.delete);
 
 // Anyone can post
