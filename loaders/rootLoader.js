bad0e0ef52abe683cd02fd6d8c11c28d329e5394
Abraham Fong <abisfong@ucsc.edu>
2021-12-08 20:05:45 -0800

    Create process variables for keys

--- loaders/rootLoader.js
+++ loaders/rootLoader.js
@@ -1,13 +1,13 @@
+require('dotenv').config({path: __dirname + '/../config/keys.env'});
 const databaseLoader = require("./databaseLoader");
-const { catchAsync } = require("../utils");
 const middlewareLoader = require("./middlewareLoaders/middlewareLoader");
 const processErrorHandlingLoader = require("./processErrorHandlingLoader");
 
-const init = catchAsync( async data => {
+const init = data => {
   databaseLoader();
   processErrorHandlingLoader();
   middlewareLoader.init(data.app);
-});
+};
 
 module.exports = {
   databaseLoader,
