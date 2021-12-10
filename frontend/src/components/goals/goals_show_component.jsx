d564373a14f55019812b640637df79d04f40f3c5
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-09 17:11:34 -0800

    Creates edit functionality to goal title and description as well as add and delete for subtasks

--- frontend/src/components/goals/goals_show_component.jsx
+++ frontend/src/components/goals/goals_show_component.jsx
@@ -4,6 +4,8 @@ class GoalsShow extends React.Component{
   constructor(props){
     super(props)
     this.state = {
+      title: this.props.goal.title,
+      description: this.props.goal.description,
       tasks: this.props.goal.tasks,
       task: '',
       toggle: false,
@@ -11,22 +13,23 @@ class GoalsShow extends React.Component{
       rerenderPlease: false
     }
     this.handleToggle = this.handleToggle.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTaskChange = this.handleTaskChange.bind(this);
+    this.handleTaskSubmit = this.handleTaskSubmit.bind(this);
+    this.handleGoalEdit = this.handleGoalEdit.bind(this);
   }
 
   handleToggle(){
     this.setState({ toggle: !this.state.toggle})
   }
 
-  handleChange(e) {
+  handleTaskChange(e) {
     e.preventDefault();
     e.stopPropagation();
     const nextState = Object.assign({}, this.state, { task: e.target.value });
     this.setState(nextState);
   }
 
-  handleSubmit(e) {
+  handleTaskSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
     const nextTasks = this.state.tasks.concat([{ body: this.state.task, completed: false }]);
@@ -41,33 +44,57 @@ class GoalsShow extends React.Component{
   //   }
   // }
 
-  handleEdit(idx) {
+  handleTaskEdit(idx) {
     return e => {
       e.preventDefault();
       e.stopPropagation();
-      const nextTasks = this.props.goal.tasks.splice(idx, 1)
+      this.setState({ editToggle: !this.state.editToggle })
     }
   }
 
-  handleDelete(idx) {
+  handleTaskDelete(idx) {
     return e => {
       e.preventDefault();
       e.stopPropagation();
       let dupeTasks = this.state.tasks;
       dupeTasks.splice(idx, 1)
-      const updatedGoal = Object.assign({}, this.props.goal, { tasks: dupeTasks })
+      const updatedGoal = Object.assign({}, { _id: this.props.goal._id, title: this.state.title, description: this.state.description, tasks: dupeTasks })
       this.props.updateGoal(updatedGoal).then(() => this.setState({ rerenderPlease: !this.state.rerenderPlease }))
+    }
+  }
+
+  handleGoalEdit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const updatedGoal = Object.assign({}, { _id: this.props.goal._id, title: this.state.title, description: this.state.description, tasks: this.state.tasks });
+    this.props.updateGoal(updatedGoal).then(() => this.setState({ editToggle: !this.state.editToggle }));
+  }
+
+  handleGoalChange(type) {
+    return e => {
+      e.preventDefault();
+      e.stopPropagation();
+      this.setState({ [type]: e.target.value })
     }
   }
 
   render(){
     return(
       <div className="goals-show">
-        <div className="goals-title">
-          <h1>{this.props.goal.title}</h1>
-          <div className="goals-description">{this.props.goal.description}</div>
-          <button onClick={this.handleToggle}>View Tasks</button> 
+        <div className="goals-title" style={!this.state.editToggle ? {display: "block"} : {display: "none"}}>
+          <h1>{this.state.title}</h1>
+          <div className="goals-description">{this.state.description}</div>
         </div>
+        <div className='goals-title-edit-form' style={this.state.editToggle ? {display: "block"} : {display: "none"}}>
+          <form onSubmit={this.handleGoalEdit}>
+            <input onChange={this.handleGoalChange('title')} value={this.state.title}/>
+            <textarea onChange={this.handleGoalChange('description')} value={this.state.description}></textarea>
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+          <button onClick={this.handleTaskEdit()}>Edit</button>
+          <br/>
+          <button onClick={this.handleToggle}>View Tasks</button> 
 
         <div className="goals-dropdown" style={this.state.toggle ? {display: "block"} : {display: "none"}}>
 
@@ -79,8 +106,8 @@ class GoalsShow extends React.Component{
                 <p>Add a task</p>
               </div> */}
               <div className='add-task-textbox'>
-                <form onSubmit={this.handleSubmit}>
-                  <textarea onChange={this.handleChange} value={this.state.task} placeholder="Add a task!"></textarea>
+                <form onSubmit={this.handleTaskSubmit}>
+                  <textarea onChange={this.handleTaskChange} value={this.state.task} placeholder="Add a task!"></textarea>
                   <button type='submit'>Submit</button>
                 </form>
               </div>
@@ -90,11 +117,21 @@ class GoalsShow extends React.Component{
               <li className="goal-tasks" key={idx}>
 
                 <div>
-                  <p>{task.body}</p>
+                  {/* <div className='task-body-text' style={!this.state.editToggle ? {display: "block"} : {display: "none"}}> */}
+                    <p>{task.body}</p>
+                  {/* </div> */}
+                  {/* <div className='task-edit' style={this.state.editToggle ? {display: "block"} : {display: "none"}}>
+                    <div className='add-task-textbox'>
+                      <form onSubmit={this.handleTaskSubmit}>
+                        <textarea onChange={this.handleTaskChange} value={this.state.task} placeholder="Add a task!"></textarea>
+                        <button type='submit'>Submit</button>
+                      </form>
+                    </div>
+                  </div> */}
                   <div>
                     {/* <button onClick={this.handleMark(idx)}>Mark Done</button> */}
-                    <button onClick={this.handleEdit(idx)}>Edit</button>
-                    <button onClick={this.handleDelete(idx)}>Delete</button>
+                    {/* <button onClick={this.handleTaskEdit(idx)}>Edit</button> */}
+                    <button onClick={this.handleTaskDelete(idx)}>Delete</button>
                   </div>
                 </div>
               </li>
