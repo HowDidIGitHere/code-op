c619c25177f7eb023a2cef2f47f03153e0f67459
ntademaru <ntademaru@gmail.com>
2021-12-10 09:25:02 -0800

    fix

--- frontend/src/components/goals/goals_create_container.js
+++ frontend/src/components/goals/goals_create_container.js
@@ -4,10 +4,10 @@ import { createGoal } from "../../actions/goal_actions";
 import GoalsCreate from "./goals_create_component";
 
 const mSTP = (state, ownProps) => {
-  const location = ownProps.location.pathname.split("/")
-  return{
-    project: state.entities.projects[location[2]],
-  }
+  // const location = ownProps.location.pathname.split("/")
+  // return{
+  //   project: state.entities.projects[location[2]],
+  // }
 }
 
 const mDTP = (dispatch, ownProps) => ({
