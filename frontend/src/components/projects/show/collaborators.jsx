47404a1114a292c1cf2fa3dcd92f409f4f7c0526
jkan@ucdavis.edu <jkan@ucdavis.edu>
2021-12-10 01:36:22 -0800

    Creates conditional to render creator in collaborators list for project show

--- frontend/src/components/projects/show/collaborators.jsx
+++ frontend/src/components/projects/show/collaborators.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 const Collaborators = props => {
   return (
     <div className='collaborator'>
+      {props.creator ? <p className='collab'><Link to={`/users/${props.creator._id}`}>{props.creator.username}</Link></p> : null}
       {props.collaborators.map((person) => 
         // <p>{person.username}</p> 
-        <Link to={`/users/${person._id}`}><p className='collab'>{person.username}</p></Link>
+        <p className='collab'><Link to={`/users/${person._id}`}>{person.username}</Link></p>
       )}
     </div>
   )
