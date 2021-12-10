16ea4a20367637507a5e38abb962e121a2f656e7
Abraham Fong <abisfong@ucsc.edu>
2021-12-09 21:07:54 -0800

    Add next error count as id to incoming tag error

---
+++ frontend/src/reducers/error_reducers/tag_errors_reducer.js
@@ -0,0 +1,28 @@
+import { 
+  RECEIVE_TAG,
+  RECEIVE_TAGS,
+  RECEIVE_TAG_ERRORS 
+} from "../../actions/user_actions";
+import { REMOVE_ERROR } from "../../actions/error_actions";
+
+const UserErrorsReducer = data => {  
+  return (state={}, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+    switch(action.type) {
+      case RECEIVE_TAGS:
+      case RECEIVE_TAG:
+        return {};
+      case RECEIVE_TAG_ERRORS:
+        nextState[data.errorCount+=1] = action.errors.message;
+        return nextState;
+      case REMOVE_ERROR:
+        if (nextState[action.id]) delete nextState[action.id]
+        return nextState;
+      default:
+        return state;
+    }
+  }
+}
+
+export default UserErrorsReducer;
