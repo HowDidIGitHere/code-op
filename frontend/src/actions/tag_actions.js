b74fa8cd5e475acdcbe21ccddc388e30e9a5dd31
Abraham Fong <abisfong@ucsc.edu>
2021-12-09 21:57:08 -0800

    Create tag error action and dispatch it on error

--- frontend/src/actions/tag_actions.js
+++ frontend/src/actions/tag_actions.js
@@ -5,6 +5,7 @@ export const RECEIVE_TAGS='RECEIVE_TAGS';
 export const RECEIVE_TAG='RECEIVE_TAG';
 export const REMOVE_TAG='REMOVE_TAG';
 export const ONE_TAG='ONE_TAG';
+export const RECEIVE_TAG_ERRORS = "RECEIVE_USER_ERRORS";
 
 
 // Actions
@@ -23,33 +24,48 @@ export const removeTag = tagId => ({
   tagId
 })
 
+export const receiveTagErrors = errors => ({
+  type: RECEIVE_TAG_ERRORS,
+  errors
+})
+
 // Thunk Action Creators
 export const fetchTags = (params) => dispatch => (
   TagApiUtil.getTags(params) 
     .then(({ data }) => dispatch(receiveTags(data)))
-    .catch(err => console.log(err))
+    .catch(({ response }) => (
+        dispatch(receiveTagErrors(response.data))
+    ))
 );
 
 export const fetchTag = id => dispatch => (
   TagApiUtil.getTag(id) 
     .then(({ data }) => dispatch(receiveTag(data)))
-    .catch(err => console.log(err))
+    .catch(({ response }) => (
+        dispatch(receiveTagErrors(response.data))
+    ))
 );
 
 export const createTag = data => dispatch => (
   TagApiUtil.createTag(data) 
     .then(({ data }) => dispatch(receiveTag(data)))
-    .catch(err => console.log(err))
+    .catch(({ response }) => (
+        dispatch(receiveTagErrors(response.data))
+    ))
 );
 
 export const updateTag = data => dispatch => (
   TagApiUtil.updateTag(data) 
     .then(({ data }) => dispatch(receiveTag(data)))
-    .catch(err => console.log(err))
+    .catch(({ response }) => (
+        dispatch(receiveTagErrors(response.data))
+    ))
 );
 
 export const deleteTag = id => dispatch => (
   TagApiUtil.deleteTag(id) 
     .then(() => dispatch(removeTag(id)))
-    .catch(err => console.log(err))
+    .catch(({ response }) => (
+        dispatch(receiveTagErrors(response.data))
+    ))
 );
