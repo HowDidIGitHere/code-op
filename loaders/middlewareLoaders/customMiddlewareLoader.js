fa4551ce12f2570c031d089b78435a85dd31f5dd
Abraham Fong <abisfong@ucsc.edu>
2021-12-14 20:31:26 -0800

    Transfer custom middleware to customMiddlewareLoader

--- loaders/middlewareLoaders/customMiddlewareLoader.js
+++ loaders/middlewareLoaders/customMiddlewareLoader.js
@@ -1,4 +1,12 @@
 module.exports = (app) => {
   // Might as well keep in
+    // Convert query array values to array object
+  app.use(function(req, res, next) {
+    for (let key in req.query)
+      if (/\w+,\w*/.test(req.query[key]))
+        req.query[key] = req.query[key].split(',');
+    next();
+  });
+  
   return app;
 }
--- loaders/middlewareLoaders/thirdPartyMiddlewareLoader.js
+++ loaders/middlewareLoaders/thirdPartyMiddlewareLoader.js
@@ -42,13 +42,5 @@ module.exports = app => {
   // Prevent parameter pollution
   app.use(hpp({ whitelist: [] }));
 
-  // Convert query array values to array object
-  app.use(function(req, res, next) {
-    for (let key in req.query)
-      if (/\w+,\w*/.test(req.query[key]))
-        req.query[key] = req.query[key].split(',');
-    next();
-  });
-
   return app;
 }
