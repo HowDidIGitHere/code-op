88cea4a9c213ff20a1dc493816a73992a9512e87
kenny <kcheng16@gmail.com>
2021-12-20 19:11:14 -0800

    project-show: add delete project to proj-show-container

--- frontend/src/components/projects/show/projects_show_container.js
+++ frontend/src/components/projects/show/projects_show_container.js
@@ -3,7 +3,7 @@ import ProjectsShow from './projects_show_component';
 import './project_show.css'
 import { fetchGoals, updateGoal } from '../../../actions/goal_actions';
 import { fetchCollaborators, fetchUser } from '../../../actions/user_actions';
-import { fetchProjects, fetchProject } from '../../../actions/project_actions';
+import { fetchProjects, fetchProject, deleteProject } from '../../../actions/project_actions';
 import { fetchDiagram, updateDiagram } from '../../../actions/diagram_actions';
 import { fetchTags } from '../../../actions/tag_actions';
 import { withRouter } from 'react-router';
@@ -21,6 +21,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     fetchProject: () => dispatch(fetchProject(ownProps.match.params.id)),
     fetchCreatorProjects: id => dispatch(fetchProjects({ creator: id })),
+    deleteProject: id => dispatch(deleteProject(id)),
     fetchCollaborators: collaboratorsArr => dispatch(fetchCollaborators(collaboratorsArr)),
     fetchGoals: goalsArr => dispatch(fetchGoals(goalsArr)),
     fetchDiagram: diagramId => dispatch(fetchDiagram(diagramId)),
