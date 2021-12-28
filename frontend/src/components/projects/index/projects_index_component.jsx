f69f07173eea03042d8879a257a86f9e99058afc
ntademaru <ntademaru@gmail.com>
2021-12-28 12:38:43 -0800

    added filter and clear to index page

--- frontend/src/components/projects/index/projects_index_component.jsx
+++ frontend/src/components/projects/index/projects_index_component.jsx
@@ -12,6 +12,7 @@ class ProjectsIndex extends React.Component {
     this.handleRequest = this.handleRequest.bind(this)
     this.handleRequest2 = this.handleRequest2.bind(this)
     this.handleSearch = this.handleSearch.bind(this)
+    this.handleClear = this.handleClear.bind(this)
   }
 
   componentWillMount() {
@@ -34,6 +35,38 @@ class ProjectsIndex extends React.Component {
     }
     this.props.fetchProjects({tags: types})
 
+  }
+
+  printSelection() {
+
+    let types = "";
+    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+    if (checkboxes.length === 0) 
+      return (
+        <div className='filters'>
+          <p className='filter-list'>None</p>
+        </div>
+      )
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (i === checkboxes.length - 1) 
+        types = types.concat(checkboxes[i].value)
+      else
+        types = types.concat(checkboxes[i].value, ", ")
+    }
+
+    return (
+      <div className='filters'>
+        <p className='filter-list'>{types}</p>
+        <button className='clear-button' onClick={this.handleClear}>Clear</button>
+      </div>
+    )
+  }
+
+  handleClear(e) {
+    e.preventDefault();
+    document.querySelectorAll('input[type="checkbox"]')
+      .forEach(el => el.checked = false);
+    this.props.fetchProjects();
   }
 
   handleRequest(e) {
@@ -68,6 +101,9 @@ class ProjectsIndex extends React.Component {
       ['Position', tags.position], 
     ]
 
+    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+    console.log(checkboxes)
+
     return(
       <div className="projects-index">
         <div className='category-filters'>
@@ -79,7 +115,12 @@ class ProjectsIndex extends React.Component {
             onClick={this.handleSearch}
           >Search</button>
         </div>
-        <div className="underline"></div>
+        <div className="underline-1"></div>
+        <div className='show-filters'>
+          <h1 className='current-filters'>Current Search Filters:</h1>
+          {this.printSelection()}
+        </div>
+        <div className="underline-2"></div>
         <div className='right-index'>
           <div className='right-index-listings'>
               {this.props.projects.map((project) => {
