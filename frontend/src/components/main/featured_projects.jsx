3c8da93aaee9265bdb5417abe397fab4fe83391e
Abraham Fong <abisfong@ucsc.edu>
2021-12-27 21:59:06 -0800

    Conditionally render projects in carousel

--- frontend/src/components/main/featured_projects.jsx
+++ frontend/src/components/main/featured_projects.jsx
@@ -5,20 +5,24 @@ export default class FeaturedProjects extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    // this.props.fetchProjects()
+  }
+
   render() {
+    const featuredProjects = this.props.featuredProjects;
     return (
       <div id="featured-projects" className="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
-          </div>
-          <div className="carousel-item">
-            <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
-          </div>
-          <div className="carousel-item">
-            <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
-          </div>
-        </div>
+        { featuredProjects ? featuredProjects.map((project, index) => {
+          return (
+            <div className="carousel-inner">
+              <div className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                <h1>project.title</h1>
+                <p>project.description</p>
+              </div>
+            </div>
+          )
+        }) : '' }
         <button className="carousel-control-prev" type="button" data-bs-target="#featured-projects" data-bs-slide="prev">
           <span className="visually-hidden">Previous</span>
         </button>
