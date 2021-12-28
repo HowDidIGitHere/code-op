a3370ee65742262f8be810779d152f69c9a7ddf9
Abraham Fong <abisfong@ucsc.edu>
2021-12-27 17:00:52 -0800

    Create a FeaturedProjects component

---
+++ frontend/src/components/main/featured_projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class FeaturedProjects extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
