2074d6f9f1b04735a6bd7ca28558fef276488d79
Abraham Fong <abisfong@ucsc.edu>
2021-12-27 18:14:49 -0800

    Use bootstrap container to make overlay responsive

--- frontend/src/components/main/splash_header.jsx
+++ frontend/src/components/main/splash_header.jsx
@@ -4,7 +4,7 @@ const SplashHeader = ({ handleClick }) => {
   return (
     <div className='splash-header'>
       <div className='banner'></div>
-      <div className='overlay'>
+      <div className='overlay container-fluid'>
         <h1>What will your next passion project be?</h1>
         <button 
           className='cta'
