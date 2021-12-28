c342f6b501afc86388f812c60bf1fa62d12b8582
Abraham Fong <abisfong@ucsc.edu>
2021-12-27 18:12:56 -0800

    Make splash header responsive

--- frontend/src/components/main/splash_header.jsx
+++ frontend/src/components/main/splash_header.jsx
@@ -4,7 +4,6 @@ const SplashHeader = ({ handleClick }) => {
   return (
     <div className='splash-header'>
       <div className='banner'></div>
-      {/* <img src="https://res.cloudinary.com/dzixdb9eu/image/upload/v1638986282/unsplash_wD1LRb9OeEo_2_me7jle.png" alt="banner" /> */}
       <div className='overlay'>
         <h1>What will your next passion project be?</h1>
         <button 
