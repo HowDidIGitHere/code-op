e8c3c2a70e7a924cbcf7b74e226c501af5e2420a
kenny <kcheng16@gmail.com>
2022-01-19 10:46:14 -0800

    learn-more: learn-more button disappears on dropdown, CSS style

--- frontend/src/components/main/how_to.jsx
+++ frontend/src/components/main/how_to.jsx
@@ -14,6 +14,14 @@ class howTo extends React.Component{
     console.log("show:", this.state.show)
   }
 
+  isLearnMoreOpen(){
+    if(this.state.show){
+      return ""
+    } else {
+      return this.toggleLearnMore()
+    } 
+  }
+
   render(){
 
     return(
@@ -23,30 +31,40 @@ class howTo extends React.Component{
             <div>Project Collaboration Reimagined</div>
             <div>We're on a mission to unite people with the right skills for the right job. Become part of a team.</div>
           </div>
-          <div className="learn-more" onClick={() => this.toggleLearnMore()}>Learn More</div>
+          <div 
+            className="learn-button"
+            style={this.state.show ? {backgroundColor: "transparent", color: "transparent", cursor: "default"} : {display: "block"}}
+            onClick={() => this.isLearnMoreOpen()}
+          >
+            <div className="learn" onClick={() => this.toggleLearnMore()}>Learn More</div>
+            <i class="fas fa-chevron-down"></i>
+          </div>
         </div>
         <div 
           className="how-to-dropdown"
           style={this.state.show ? {display: "block"} : {display: "none"}}
         >
-          <div>How Code-Op Works</div>
-          <div>Code-op builds strong development and production teams by attracting experts from different fields and uniting them to complete projects faster.</div>
+          <div id="title">How Code-Op Works</div>
+          <div id="desc">Code-op builds strong development and production teams by attracting experts from different fields and uniting them to complete projects faster.</div>
           <div className="cards-container">
-            <div className="cards">
-              <div className="card-header">Endless types projects:</div>
+            <div className="learn-card">
+              <div className="card-title">Endless types projects:</div>
               <div>With Code-Op, you can filter through a variety of different projects based on your language skills to join the project that fits you.</div>
             </div>
-            <div className="cards">
-              <div className="card-header">Send a request:</div>
+            <div className="learn-card">
+              <div className="card-title">Send a request:</div>
               <div>See a fun or interesting project you like? Make your own request to join it and build something big!</div>
             </div>
-            <div className="cards">
-              <div className="card-header">Build your own team:</div>
+            <div className="learn-card">
+              <div className="card-title">Build your own team:</div>
               <div>Work on what you want by creating your own project and accepting users that meet your needs.</div>
             </div>
           </div>
         </div>
-        <div>up arrow</div>
+        <i 
+          className="fas fa-chevron-up"
+          style={this.state.show ? {display: "block"} : {display: "none"}}
+        ></i>
       </div>
     )
   }
